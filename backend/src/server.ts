a77c7b6fb673ae64c8fc2e0e5f2e0f911aa30d4e
Kevin <kolioane@gmail.com>
2026-03-03 00:15:23 -0800

    Init server.ts; update package.json; install cors, body-parser, express and its @types modules

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+import { handler as expensesHandler } from '../lambdas/expenses/index';
+import { handler as groupsHandler } from '../lambdas/groups/index';
+import { handler as receiptsHandler } from '../lambdas/receipts/index';
+import { handler as analyticsHandler } from '../lambdas/analytics/index';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Environment
+process.env.TABLE_NAME = 'CostCrunchTable';
+process.env.RECEIPTS_BUCKET = 'costcrunch-receipts';
+process.env.ENVIRONMENT = 'dev';
+process.env.LOG_LEVEL = 'debug';
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
