e8f34eb2d0451564c01af19b062835bea04638e5
Kevin <kolioane@gmail.com>
2026-03-11 23:56:52 -0700

    setup vitest for frontend

--- frontend/__tests__/setup.ts
+++ frontend/__tests__/setup.ts
@@ -1,7 +1,10 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
+import { afterEach, expect } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+
+// Extend Vitest's expect with Jest DOM matchers
+expect.extend(matchers);
 
 afterEach(() => {
-  cleanup()
-})
+  cleanup();
+});
